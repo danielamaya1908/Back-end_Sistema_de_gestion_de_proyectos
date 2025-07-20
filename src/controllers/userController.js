23c60d6377ba8701db84c82eeb6870ebcc0b70dd
DANIEL ENOC AMAYA AMAYA <130795116+danielamaya1908@users.noreply.github.com>
2025-07-20 18:13:50 -0500

    req

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,7 +9,7 @@ import { verifyToken, authorizeRoles } from "../middlewares/auth.middleware.js";
 
 export const getAllUsers = async (req, res) => {
   try {
-    const result = await getAllUsersService(req.body);
+    const result = await getAllUsersService(req.query);
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ message: error.message });
